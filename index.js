0f29cfa9447c376911447f5b07794f8c59dc1c3a
Grace Kang <gracekang.93@gmail.com>
2018-08-13 12:44:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function getFirstSelector(selector) {
+  return document.getElementsByTagName(selector)[0];
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').getElementsByClassName('target');
+}
+
+function increaseRankBy(n) {
+  //increases the ranks in all of the .ranked-list's by n;
+}
+
+function deepestChild() {
+  //pulls out the most deeply nested child from div#grand-node
+}
